66d821f8ac4c24021525ae93b33a6778fa2dc573
Kaylindaww <79210939+Kaylindaww@users.noreply.github.com>
2021-03-21 16:21:45 +0300

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,12 @@
+function students(){ 
+var a="shadya";
+var b="pamela";
+function identity(){
+    var c = a+
+    " " + b +  " are friends";
+    console.log(c);
+}
+identity();
+}
+
+ students();
